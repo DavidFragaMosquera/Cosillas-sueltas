67721f792867aa9337b88d3a8b3cf45cf7786fc1
DavidFragaMosquera <davidrosales1990@gmail.com>
2020-03-08 22:45:12 +0100

    conversor de monedas

---
+++ JSII/www/js/main.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll(".value");
+
+const url =
+  "https://api.exchangeratesapi.io/latest?symbols=USD,GBP,JPY,MXN,CHF";
+fetch(url)
+  .then(euro => euro.json())
+  .then(data => {
+    document.querySelector("#USD").dataset.cambio = data.rates.USD;
+    document.querySelector("#GBP").dataset.cambio = data.rates.GBP;
+    document.querySelector("#JPY").dataset.cambio = data.rates.JPY;
+    document.querySelector("#MXN").dataset.cambio = data.rates.MXN;
+    document.querySelector("#CHF").dataset.cambio = data.rates.CHF;
+
+    inputs.forEach(input => {
+      input.value = input.dataset.cambio;
+    });
+  });
+
+function valorCambiado(input) {
+  const factor = input.value / input.dataset.cambio;
+  inputs.forEach(campo => {
+    campo.value = (campo.dataset.cambio * factor).toFixed(2);
+  });
+}
