f5559e22162738f8eb7ba1f6358407b1564dfad6
DavidFragaMosquera <davidrosales1990@gmail.com>
2020-05-04 10:27:29 +0200

    Mejora conversor binario

--- JS/entrega-3/conversionBinario.js
+++ JS/entrega-3/conversionBinario.js
@@ -1,5 +1,28 @@
 "use strict";
 
+// Con bucle
+
+const numeroBinario = "1010";
+
+function binaryConverter(str) {
+  let result = 0;
+  let exp = 0;
+  
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i] * 2 ** exp++;
+  }
+  return result;
+}
+
+console.log(binaryConverter(numeroBinario));
+
+
+//****************************************************************//
+
+
+// con parseInt
+
 let binario;
 binario = prompt('Introduce un numero binario');
 const decimal = parseInt(binario, 2);
